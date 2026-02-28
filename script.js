25b3a45335dd4bf031d1927b841e832957ce16a8
25ln0526ms <25ln0526_ms@psu.edu.ph>
2026-02-28 12:59:59 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -8,27 +8,27 @@ const closeBtn = document.querySelector(".close");
 
 // DESCRIPTION DATA (single source of truth)
 const imageData = {
-  "pic.png":{
+  "pic.jpg":{
     title: "Shared Laughter",
     desc:
     "This image captures a group of friends descending the stairs, engaged in conversation. The smiles on their faces reflect the joy of being together, even as their attention momentarily shifts to carefully navigating each step. It is a simple, fleeting moment that reveals both connection and mindfulness in everyday life.",
   },
-  "pic1.png":{
+  "pic1.jpg":{
     title: "Shared Space",
     desc:
     "The image shows a group of male students walking together in a covered school hallway. They are wearing school uniforms and carrying backpacks. While walking side by side, all of them are looking down at their cellphones instead of fully paying attention to where they are going.",
   },
-  "pic2.png":{
+  "pic2.jpg":{
     title: "Focused Mind",
     desc:
     "The main focus of the image is the student, completely immersed in his book. Though there are people beside him and having the noisy environment, he remains undisturbed. This scene highlights the theme of concentration amidst distraction emphasizing his dedication and seriousness toward learning.",
   },
-  "pic3.png":{
+  "pic3.jpg":{
     title: "Campus FLow",
   desc:
     "The image captures the front entrance of the campus filled with movement and everyday activity. Students and visitors are seen arriving and leaving at the same time. Some are getting off motorcycles, others are about to ride, while a few are walking or driving past the gate. The guard stands attentively, checking those who enter the school. This scene highlights the rhythm of campus life, showing a space where different paths, purposes, and moments intersect all at once.",
   },
-  "pic4.png":{
+  "pic4.jpg":{
     title: "In Their Element",
     desc:
     "This photo captures a vibrant, candid scene within a university pavilion. The space is crowded with students involved in various activities, ranging from socializing and sharing laughs to browsing on their phones and capturing photographs."
@@ -44,7 +44,7 @@ const imageData = {
   modalImg.classList.remove("full-image");
 
   // ✅ ONLY pic3.png shows full image
-  if (imageSrc === "pic3.png") {
+  if (imageSrc === "pic3.jpg") {
     modalImg.classList.add("full-image");
   }
 
