8a6f45c0ee455c2911716d7fcf1495b91a587a7d
25ln0526ms <25ln0526_ms@psu.edu.ph>
2026-03-21 21:09:20 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -19,19 +19,19 @@ const imageData = {
     "The image shows a group of male students walking together in a covered school hallway. They are wearing school uniforms and carrying backpacks. While walking side by side, all of them are looking down at their cellphones instead of fully paying attention to where they are going.",
   },
   "pic2.jpg":{
-    title: "Focused Mind",
+    title: "Class Whisper",
     desc:
-    "The main focus of the image is the student, completely immersed in his book. Though there are people beside him and having the noisy environment, he remains undisturbed. This scene highlights the theme of concentration amidst distraction emphasizing his dedication and seriousness toward learning.",
+    "This image shows what some students do in the middle of a class. The student on the left is listening to the lesson while also paying attention to his seatmate. In the photo, he is facing the front where a monitor shows the lesson of the day, but his eyes were captured looking at his seatmate. The student on the right is talking to him, which causes him to divide his attention. Even though he is listening to his seatmate, he can still fulfill his responsibility as a student, as he is still able to understand the lesson.",
   },
   "pic3.jpg":{
-    title: "Campus FLow",
+    title: "Peer Gathering",
   desc:
-    "The image captures the front entrance of the campus filled with movement and everyday activity. Students and visitors are seen arriving and leaving at the same time. Some are getting off motorcycles, others are about to ride, while a few are walking or driving past the gate. The guard stands attentively, checking those who enter the school. This scene highlights the rhythm of campus life, showing a space where different paths, purposes, and moments intersect all at once.",
+    "This image shows a group of students seated in a straight line behind a long table, inspired by The Last Supper, with one student at the center and others on both sides creating balance. In front of them are sliced green mangoes, cups, and dipping sauce, showing they are sharing food in a casual school setting, while some students talk or look at each other. This connects to intertextuality because the image copies the arrangement of a famous artwork and presents it in a modern situation, helping viewers recognize the reference and understand its deeper meaning.",
   },
   "pic4.jpg":{
     title: "In Their Element",
     desc:
-    "This photo captures a vibrant, candid scene within a university pavilion. The space is crowded with students involved in various activities, ranging from socializing and sharing laughs to browsing on their phones and capturing photographs."
+    "The photo shows a group of students gathered in one place, doing different things like talking, walking, taking pictures, and sitting. Because of the way the people are arranged and the presence of the columns, it can remind viewers of The School of Athens, where many individuals are also grouped together in a shared space, each doing their own thing but still part of the scene.",
   }
   };
 
